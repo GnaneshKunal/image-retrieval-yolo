9896315272a8cede2204545952209dd4b0befad4
Gnanesh Kunal <gnaneshkunal@outlook.com>
2018-03-28 01:10:35 +0530

    yolo d

--- yolo.d/train.c
+++ yolo.d/train.c
@@ -7,10 +7,13 @@
 #include <sys/types.h>
 #include <stdbool.h>
 #include <omp.h>
+#include <mongoc.h>
+#include <bcon.h>
 
 #define MAX_LINE 4096
 
 void err_quit(const char *);
+void do_json(char *, char *, char*, char*);
 
 int main(void) {
   DIR *dp;
@@ -64,7 +67,50 @@ int main(void) {
     }
     puts("DATA contains: ");
     puts(data);
+    char *json = malloc(1000);
+    char dirname[100];
+    strcpy(dirname, darknet);
+    strcat(dirname, dirp->d_name);
+    char dirnamep[100];
+    strcpy(dirnamep, newPredictions);
+
+    do_json(json, data, dirname, newPredictions);
+
+    mongoc_client_t * client;
+    mongoc_collection_t * collection;
+    bson_error_t error;
+    bson_oid_t oid;
+    bson_t *doc;
+
+    mongoc_init();
+
+    client = mongoc_client_new("mongodb://localhost:27017/?appname=cbir-yolo");
+    collection = mongoc_client_get_collection(client, "cbir", "yolo");
+
+    bson_t *bson;
+    char *string;
+    bson = bson_new_from_json((const uint8_t *)json, -1, &error);
+
+    if (!bson) {
+      fprintf(stderr, "%s\n", error.message);
+      return EXIT_FAILURE;
+    }
+
+    string = bson_as_canonical_extended_json(bson, NULL);
+    printf("%s\n", string);
+
+    if (!mongoc_collection_insert_one(
+                                      collection, bson, NULL, NULL, &error)) {
+      fprintf(stderr, "%s\n", error.message);
+    }
+
+    bson_free(string);
+    mongoc_collection_destroy(collection);
+    mongoc_client_destroy(client);
+    mongoc_cleanup();
+
     free(data);
+    free(json);
     pclose(fp);
 
   }
@@ -75,5 +121,41 @@ int main(void) {
 void err_quit(const char *fmt) {
   puts(fmt);
   exit(1);
+}
+
+void do_json(char *json, char *data, char *dir, char *dirp) {
+  char *fn = strchr(data, '\n');
+  strcpy(json, "{");
+  strcat(json, "\"dir\": \"");
+  strcat(json, dir);
+  strcat(json, "\", \"dirp\":\"");
+  strcat(json, dirp);
+  strcat(json, "\", \"predicted\": [");
+
+  while ((fn = strchr(fn, '\n') + 1) != NULL && strlen(fn) != 0) {
+    char *end2 = strchr(fn, ':');
+    int k2 = end2 - fn;
+    char *buf2 = (char *)malloc(k2 * sizeof(char));
+    strncpy(buf2, fn, k2);
+    buf2[k2] = '\0';
+    strcat(json, "{\"");
+    strcat(json, buf2);
+    strcat(json, "\": \"");
+
+    char * start3 = strchr(fn, ':') + 2;
+    char * end3 = strchr(fn, '\n');
+    int k3 = end3 - start3;
+    char *buf3 = (char *)malloc(k3 * sizeof(char));
+    strncpy(buf3, start3, k3);
+    buf3[k3] = '\0';
+    strcat(json, buf3);
+    strcat(json, "\"}");
+
+    strcat(json, ",");
+  }
+
+  json[strlen(json) - 1] = ' ';
+  strcat(json, "]");
+  strcat(json, "}");
 }
 
